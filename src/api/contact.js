ea8523dc8fd27aa54814521d2e132d23d0eb75cf
andrew <the.fanatic.explorer@gmail.com>
2026-06-22 16:24:41 +0200

    Fix contact API paths to use /content prefix

--- src/api/contact.js
+++ src/api/contact.js
@@ -1,5 +1,5 @@
 import api from './index.js'
 
-export const sendContactMessage = (data) => api.post('/contact', data)
-export const getContactMessages = (params, config = {}) => api.get('/admin/contact-messages', { params, ...config })
-export const markContactMessageRead = (id) => api.patch(`/admin/contact-messages/${id}/read`)
+export const sendContactMessage = (data) => api.post('/content/contact', data)
+export const getContactMessages = (params, config = {}) => api.get('/content/contact-messages', { params, ...config })
+export const markContactMessageRead = (id) => api.patch(`/content/contact-messages/${id}/read`)
