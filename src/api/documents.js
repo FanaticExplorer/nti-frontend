8890a35c117fedd6f5e9f87c8cb5aabbbd139d5a
andrew <the.fanatic.explorer@gmail.com>
2026-06-22 23:37:07 +0200

    Fix document upload: send application_id as form field instead of query param

--- src/api/documents.js
+++ src/api/documents.js
@@ -1,6 +1,9 @@
 import api from './index.js'
 
 export const getDocument = (id, config = {}) => api.get(`/documents/${id}`, { ...{ responseType: 'blob' }, ...config })
-export const uploadDocument = (applicationId, formData) => api.post(`/documents?application_id=${applicationId}`, formData, {
-  headers: { 'Content-Type': 'multipart/form-data' }
-})
+export const uploadDocument = (applicationId, formData) => {
+  formData.append('application_id', applicationId)
+  return api.post('/documents', formData, {
+    headers: { 'Content-Type': 'multipart/form-data' }
+  })
+}
